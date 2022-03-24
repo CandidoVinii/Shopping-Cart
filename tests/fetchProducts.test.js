bab5f06c32983d58477cd54ba467aa7b7f947fa0
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-03-24 17:27:11 -0300

    Resolvido uma parte da cobertura de testes

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,16 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fecthProducts', () => {
   it('Testa se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
-  })
-  fail('Teste vazio');
+  });
+  it('Testa se o fecth foi chamado correttamente', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Testa se a função retorna o endPoint Correto', async () => {
+    expect.assertions(1);
+    const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(endPoint);
+  });
 });
